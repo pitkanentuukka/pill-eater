9c41cde492dc6f45a48bf7b39d36a62a24e6aa08
Tuukka Pitk?nen <58333519+pitkanentuukka@users.noreply.github.com>
2013-10-23 10:07:35 +0300

    resized loading text

--- src/com/tuukka/fromscratch/SplashScene.java
+++ src/com/tuukka/fromscratch/SplashScene.java
@@ -15,11 +15,15 @@ public class SplashScene extends BaseScene {
 		this.CAMERA_HEIGHT = 480;
 		this.CAMERA_WIDTH = 800;
 		// TODO Auto-generated method stub
-		setBackground(new Background(Color.WHITE));
+		setBackground(new Background(Color.BLACK));
 		text = new Text(0,0, ResourcesManager.getInstance().font,"loading", vbom);
+		text.setScale(7.5f);
+		//text.setSize(500, 300);
+		
 	    //attachChild(new Text(400, 240, resourcesManager.font, "Loading...", vbom));
 		this.attachChild(text);
-		text.setPosition(CAMERA_WIDTH/2-(text.getWidth()), CAMERA_HEIGHT/2-(text.getHeight()));
+		//text.setPosition(CAMERA_WIDTH/2-(text.getWidth()), CAMERA_HEIGHT/2-(text.getHeight()));
+		text.setPosition(CAMERA_WIDTH/2, CAMERA_HEIGHT/2);
 
 	}
 
